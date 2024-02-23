9e41fa9bd9350f00eba072d77a1e3bd9d88354c8
thanmayee-bs <161021622+thanmayee-bs@users.noreply.github.com>
2024-02-23 16:13:19 +0530

    Add files via upload

---
+++ collections/hashm.java
@@ -0,0 +1,15 @@
+package collections;
+import java.util.*;
+public class hashm {
+
+    public static void main(String[] args)
+    {
+        HashMap<Integer,String> hm = new HashMap<Integer, String>();
+
+        hm.put(1,"abc");
+        hm.put(2,"xyz");
+        
+
+
+    }
+}
